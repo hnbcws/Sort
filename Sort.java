180351f8e5f66828c525644023e509c995c6b6e5
hnbcws <1823487630@qq.com>
2019-03-05 17:32:08 +0800

    add bubble select insert sort

--- Sort.java
+++ Sort.java
@@ -5,9 +5,83 @@ public class Sort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-         System.out.println("添加了打印语句");
-         System.out.println("add print again");
+		
+		int[] arr= new int[]{8,17,6,5,34,22,12};
+		//冒泡排序
+	/** bubbleSort(arr,7);
+		System.out.println("冒泡排序:");
+		print(arr);
+	    //选择排序
+
+        selectSort(arr,7);
+		System.out.println("选择排序:");
+		print(arr);
+	**/	
+		
+		insertSort(arr,7);
+		System.out.println("插入排序:");
+		print(arr);
+	
+     
+		
+	
 	}
+	
+    public static void bubbleSort(int[]a,int n){
+     if(n<=1) return;
+	 for(int i=0;i<n-1;i++){
+		 boolean flag =false;
+		 for(int j=0;j<n-i-1;j++){
+			 if(a[j]>a[j+1]){
+			     int temp=a[j];
+				 a[j]=a[j+1];
+				 a[j+1]=temp;
+				 flag=true;
+			 }
+		 }
+		 if(!flag) break;
+	 }
+	}
+    
+    public static void selectSort(int[]a,int n){
+    	if(n<=1) return;
+    	for(int i=0;i<n-1;i++){
+    		int pos=i;  //pos 每一轮比较最小值的位置 
+    		for(int j=i+1;j<n;j++){
+    			if(a[pos]>a[j]){
+    				pos=j;
+    			}
+    		}
+    		if(i!=pos){
+    			int temp =a[i];
+    			a[i]=a[pos];
+    			a[pos]=temp;
+    		}
+    		
+    	}
+    }
+    
+    public static void insertSort(int[]a,int n){
+    	if(n<=1) return;
+    	for(int i=1;i<n;i++){
+    	  for(int j=i;j>0;j--){
+    		  if(a[j]<a[j-1]){
+    			  int temp =a[j];
+    			  a[j]=a[j-1];
+    			  a[j-1]=temp;
+    		  }else{
+    			  break;
+    		  }
+    		  
+    	  }
+    		
+    	}
+    }
+    
+    public static void print(int[] a){
+    	for(int i=0;i<a.length;i++){
+			System.out.print(a[i]+" ");
+		}
+    }
 
 }
