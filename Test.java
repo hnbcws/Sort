bce36614d9d9c0a4f13cfdb137ace99b98f650a4
hnbcws <1823487630@qq.com>
2019-03-05 11:16:24 +0800

    add test.java

---
+++ Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("我是test分支下的java文件");
+	}
+
+}
